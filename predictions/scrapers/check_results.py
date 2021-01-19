3f057ef525a8d354e13ff1c4343a64dc0787470f
velinovasen <velinovasen@users.noreply.github.com>
2021-01-19 14:49:25 +0200

    'fixed_results'

--- predictions/scrapers/check_results.py
+++ predictions/scrapers/check_results.py
@@ -34,7 +34,8 @@ class Results:
     }
 
     REGEX = {
-        "score": r'[t][a][b][l][e]\-[s][c][o][r][e]\"\>(\d{1,2}\:\d{1,2}|\d{1,2}\:\d{1,2}.+|[p][o][s][t][p][.])\<\/',
+        "score": r'[t][a][b][l][e]\-[s][c][o][r][e]\"\>(\d{1,2}\:\d{1,2}[ ][p][e][n]\.|\d{1,2}\:\d{1,2}|'
+                 r'[p][o][s][t][p][.]|\d{1,2}\:\d{1,2}.+)\<\/[t][d]\>.{1,400}\<[t][d]',
         "both_teams_draw": r'\/\"\>([A-z0-9].{1,40})[ ]\-[ ]([A-z0-9].{1,40})\<\/[a]',
         "home_won": r'[s][p][a][n][ ][c][l][a][s][s]\=\"[b][o][l][d]\"\>([A-z0-9].{1,40})\<\/[s][p][a][n]',
         "home_loosing": r'\/\"\>([A-z0-9].{1,40})[ ]\-[ ]',
@@ -73,92 +74,149 @@ class Results:
         games = soup.find_all('tr')
         return games
 
+    @staticmethod
+    def check_for_winner(data_str):
+        regex = r'[o][d]{2}[s]\-[n][o][w][r][p][ ][r][e][s][u][l][t]\-[o][k]|[o][d]{2}[s]\-[n][o][w][r][p]'
+        result = re.findall(regex, str(data_str))
+        winner = result.index('odds-nowrp result-ok')
+        if winner == 0:
+            return '1'
+        elif winner == 1:
+            return 'X'
+        elif winner == 2:
+            return '2'
+        else:
+            return 'pst'
+
     def clean_data(self, games, link):
         # CLEAN THE DATA
         for game in games:
-            score = re.search(self.REGEX['score'], str(game))
             print(game)
-            print('--------------------------------------------------------')
+            score = re.findall(self.REGEX['score'], str(game))
+            print(score)
+           # print(score)
+           # print('--------------------------------------------------------')
             try:
                 if score:
-                    score = score.group(1)
-                    [home_score, away_score] = score.split(':')
+                    winner_odds_list = []
+                    # GATHER THE ODDS AND CHECK BY THE CLASS NAME WHICH TEAM IS THE WINNER
+                   # print(f"GORNIQ {score.group(1)[:10].split('<')[0]}")
+                    score = score[0][:20].split('<')[0]
+                    print(score)
+                    home_score, away_score = '', ''
                     home_team, away_team = '', ''
+                    print('pen' in score or 'ET' in score)
+                    if 'pen' in score or 'ET' in score:
+                        home_score, away_score = score.split(' ')[0].split(':')
+                        print(away_score.split('\xa0'))
+                        away_score = away_score.split('\xa0')[0]
+                        print('ima interval')
+                    elif score == 'postp.':
+                        print('-------------------OTLOJEN')
+                        score = 'postp.'
+                    else:
+                        home_score, away_score = score.split(':')
+                        print(score.split(':'))
+                    print(f"{home_score} ---- {away_score}")
+                    print(score)
                     time = re.search(self.REGEX['time'], str(game)).group(1)
                     if link == 'today':
                         date_model = date.today().strftime('%Y-%m-%d')
                     else:
                         date_model = (date.today() - timedelta(hours=24)).strftime('%Y-%m-%d')
-
-                    if home_score > away_score:
-                        home_team = re.search(self.REGEX['home_won'], str(game)).group(1)
-                        away_team = re.search(self.REGEX['away_loosing'], str(game)).group(1)
-                        #print(f'{home_team} {score} {away_team}')
-                    elif home_score == away_score:
-                        tokens = re.search(self.REGEX['both_teams_draw'], str(game))
-                        home_team, away_team = tokens.group(1), tokens.group(2)
-                        #print(f'{home_team} {score} {away_team}')
-                    else:
-                        home_team = re.search(self.REGEX['home_loosing'], str(game)).group(1)
-                        away_team = re.search(self.REGEX['away_winning'], str(game)).group(1)
-                        #print(f'{home_team} {score} {away_team}')
+                    print(time, date_model)
+                    if score != 'postp.':
+                        print(type(home_score), type(away_score))
+                        print(home_score, away_score)
+                        if 'ET' in score or 'pen' in score or int(home_score) == int(away_score):
+                            tokens = re.search(self.REGEX['both_teams_draw'], str(game))
+                            home_team, away_team = tokens.group(1), tokens.group(2)
+                        elif int(home_score) > int(away_score):
+                            home_team = re.search(self.REGEX['home_won'], str(game)).group(1)
+                            away_team = re.search(self.REGEX['away_loosing'], str(game)).group(1)
+                            # print(f'{home_team} {score} {away_team}')
+                        else:
+                            home_team = re.search(self.REGEX['home_loosing'], str(game)).group(1)
+                            away_team = re.search(self.REGEX['away_winning'], str(game)).group(1)
+                            # print(f'{home_team} {score} {away_team}')
+                        print(f'{home_team} {score} {away_team}')
+                    # GET THE GAMES AND UPDATE THE STATUS AND SCORE
+                    # print('idva do tuka')
 
                     if Game.objects.get(date=date_model, time=time, home_team=home_team, away_team=away_team):
-                        game = Game.objects.get(date=date_model, time=time, home_team=home_team, away_team=away_team)
-                        if game.winner == '-':          # UPDATE THE INFO IF ITS FINISHED AND NOT UPDATED
-                            if int(home_score) > int(away_score):
-                                game.winner = '1'
-                            elif int(home_score) < int(away_score):
-                                game.winner = '2'
-                            else:
-                                game.winner = 'X'
-                            game.score = score
-                            game.status = 'finished'
-                            game.save(update_fields=['winner', 'score', 'status'])
-                            #print('UPDATED')
+                        new_game = Game.objects.get(date=date_model, time=time, home_team=home_team, away_team=away_team)
+                       # print(f'REZULTATA {score}')
+                        print(new_game)
+                        print(new_game.status == 'not played')
+                        if new_game.status == 'not played':          # UPDATE THE INFO IF ITS FINISHED AND NOT UPDATED
+                            print('PREDI FUNKCIQTA')
+                            print(self.check_for_winner(game))
+                            print('SLED NEQ')
+                            new_game.winner = self.check_for_winner(game)
+                            new_game.score = score[:6]
+                            new_game.status = 'finished'
+                            new_game.save(update_fields=['winner', 'score', 'status'])
+                            print('UPDATED')
                         else:
                             #print('still not finished')
                             pass
 
-                        users_predictions = game.game_predicted.all()        # GET THE USER PREDICTIONS
-
-                        for prediction in users_predictions:     # CHECK THE PREDICTIONS STATUS AND UPDATE
-                            print(prediction.status, prediction.odd, prediction.home_team, prediction.away_team)
-                            if prediction.status == 'pending':
-                                if game.winner == prediction.sign:
-                                    prediction.status = 'won'
-                                else:
-                                    prediction.status = 'lost'
-                                prediction.score = score
-                            prediction.save()
-
-                        users_bets = game.bet_game.all()  # GET ALL BETS
-
+                        users_bets = new_game.bet_game.all()  # GET ALL BETS
+                        print(users_bets)
+                        #print(game)
                         for user_bet in users_bets:              # CHECK THE BETS STATUS AND UPDATE
-                            print(user_bet.status, user_bet.bet_user.percent_profit)
+                          #  print(user_bet.status, user_bet.bet_user.percent_profit)
                             total_cash = 0
+                            print('VLIZAME')
                             if user_bet.status == 2:
-                                if game.winner == user_bet.bet_sign:
+                                if new_game.winner == user_bet.bet_sign:
                                     user_bet.status = 1
                                     curr_amount = float(user_bet.bet_user.cash)
                                     user_bet.bet_user.cash = curr_amount + float(user_bet.bet_odd * float(user_bet.bet_amount))
                                     total_cash = curr_amount + float(user_bet.bet_odd * float(user_bet.bet_amount))
+                                elif new_game.winner == 'postp.':
+                                    user_bet.status = 1
+                                    curr_amount = float(user_bet.bet_user.cash)
+                                    user_bet.bet_user.cash = curr_amount + float(1 * float(user_bet.bet_amount))
+                                    total_cash = curr_amount + float(1 * float(user_bet.bet_amount))
                                 else:
                                     user_bet.status = 0
                                     total_cash = float(user_bet.bet_odd * float(user_bet.bet_amount))
-                                print(f'{total_cash} -- {total_cash / 500 * 100}')
+                               # print(f'{total_cash} -- {total_cash / 500 * 100}')
                                 if total_cash < 500:
                                     user_bet.bet_user.percent_profit = f'{-(100 - total_cash / 500 * 100):.2f}'
                                 else:
                                     user_bet.bet_user.percent_profit = f'{(total_cash / 500 * 100) - 100:.2f}'
-                                user_bet.score = score
+                                user_bet.score = score[:6]
                             user_bet.save()
                             user_bet.bet_user.save()
 
+
             except Exception as e:
-                print(e)
+              pass
+              #  print(e)
 
 
-if __name__ == '__main__':
-    tmr = Results()
-    tmr.scrape()
+
+# if __name__ == '__main__':
+#     tmr = Results()
+#     tmr.scrape()
+
+
+
+# CURRENTLY REMOVED
+
+# users_predictions = new_game.game_predicted.all()        # GET THE USER PREDICTIONS
+#
+# for prediction in users_predictions:     # CHECK THE PREDICTIONS STATUS AND UPDATE
+#    # print(prediction.status, prediction.odd, prediction.home_team, prediction.away_team)
+#     if prediction.status == 'pending':
+#         if game.winner == prediction.sign:
+#             prediction.status = 'won'
+#         elif game.winner == 'postp.':
+#             prediction.status = 'won'
+#             prediction.odd = 1.00
+#         else:
+#             prediction.status = 'lost'
+#         prediction.score = score
+#     prediction.save()
